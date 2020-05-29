2935d1a1691bb8374774e283002a07fc4f898dd1
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-05-29 12:56:58 -0300

    Logramos hacer andar los mocks

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -3,24 +3,22 @@ from auth_server.db_functions import insert_into_users_db
 import simplejson as json
 
 def test_register_user_succesfully(client):
-	with patch('auth_server.db_functions.insert_into_users_db') as mock:
+	with patch('auth_server.authentication.insert_into_users_db') as mock:
 		user_information = {'email': 'segundo_intento_this_email_should_not_be_saved@test.com',
 				'password': 'fake password',
 				'first name': 'first name', 'last name': 'last name',
 				'phone number': 'phone number', 'profile picture': 'profile picture',
 				'hash': 'hash', 'salt': 'salt'}
 
-		mock.return_value.json.return_value = \
-			{'Registration' :
-			'Successfully registered new user with email {0}'.format(user_information['email'])}
-		mock.return_value.status_code = 200
+		result = {'Registration': 'Successfully registered new user with email {0}'.format(user_information['email'])}
+		status_code = 201
+
+		mock.return_value = result, status_code
 
 		response = client.post('/api/register/', json=user_information,
 							   follow_redirects=False)
 		value_expected = {'Registration' :
 			'Successfully registered new user with email {0}'.format(user_information['email'])}
 
-		assert True
 		assert mock.called
-		# assert mock_register_user.assert_not_called
-		# assert json.loads(response.data) == value_expected
+		assert json.loads(response.data) == value_expected
