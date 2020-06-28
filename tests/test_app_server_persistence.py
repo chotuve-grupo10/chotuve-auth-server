bfae278b9296351e7289eff300c6de2422d0c342
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-06-27 22:36:40 -0300

    Un test mas para chequear el get by token

--- tests/test_app_server_persistence.py
+++ tests/test_app_server_persistence.py
@@ -31,6 +31,20 @@ def test_app_server_with_given_token_not_found(postgresql_db):
     with pytest.raises(AppServerNotFoundException):
         user = sut.get_app_server_by_token('5a3d6026-2f5c-4957-b52d-c094b50774db')
 
+def test_get_app_server_by_token_successfully(postgresql_db):
+    session = postgresql_db.session
+    create_appservers_table(session)
+    assert query_first_app_server(session) is None
+
+    app_server_to_save = AppServer()
+    sut = AppServerPersistence(postgresql_db)
+    sut.save(app_server_to_save)
+    row = query_first_app_server(session)
+    assert row is not None
+    assert row[1].month == datetime.datetime.now().month
+
+    assert sut.get_app_server_by_token(app_server_to_save.get_token()) is not None
+
 def test_delete_app_server_successfully(postgresql_db):
     session = postgresql_db.session
     create_appservers_table(session)
