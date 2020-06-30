14112f12167af77c6f4f6dbf5894b3a646458ee2
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-06-30 15:58:12 -0300

    Agrego metodo para saber si el user es admin o no

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,3 +5,11 @@ def test_is_firebase():
   user_not_firebase = User('aa@gmail.com', 'pa$$word', 'John Doe', '1111', 'NULL', False, False)
   assert user_firebase.is_firebase_user()
   assert not user_not_firebase.is_firebase_user()
+
+def test_is_not_admin_user():
+  user = User('aa@gmail.com', 'pa$$word', 'John Doe', '1111', 'NULL', True, False)
+  assert not user.is_admin_user()
+
+def test_is_admin_user():
+  user = User('aa@gmail.com', 'pa$$word', 'John Doe', '1111', 'NULL', False, True)
+  assert user.is_admin_user()
