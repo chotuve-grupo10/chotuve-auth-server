c2277849dff7c395b3dd00bbe0b446249efb64de
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-07-14 15:36:03 -0300

    Van tests para probar el obtener por mail

--- tests/test_reset_password_persistence.py
+++ tests/test_reset_password_persistence.py
@@ -32,26 +32,27 @@ def test_save_reset_password_successfully(postgresql_db):
     assert row is not None
     assert row[1] == email
 
-# def test_app_server_with_given_token_not_found(postgresql_db):
-#     session = postgresql_db.session
-#     create_appservers_table(session)
-#     sut = AppServerPersistence(postgresql_db)
-#     with pytest.raises(AppServerNotFoundException):
-#         user = sut.get_app_server_by_token('5a3d6026-2f5c-4957-b52d-c094b50774db')
+def test_reset_password_with_given_email_not_found(postgresql_db):
+    session = postgresql_db.session
+    create_reset_password_table(session)
+    sut = ResetPasswordPersistence(postgresql_db)
+    with pytest.raises(ResetPasswordNotFoundException):
+        user = sut.get_reset_password_by_email('hola@hola.com')
 
-# def test_get_app_server_by_token_successfully(postgresql_db):
-#     session = postgresql_db.session
-#     create_appservers_table(session)
-#     assert query_first_app_server(session) is None
+def test_get_reset_password_by_email_successfully(postgresql_db):
+    session = postgresql_db.session
+    create_reset_password_table(session)
+    assert query_first_reset_password(session) is None
 
-#     app_server_to_save = AppServer()
-#     sut = AppServerPersistence(postgresql_db)
-#     sut.save(app_server_to_save)
-#     row = query_first_app_server(session)
-#     assert row is not None
-#     assert row[1].month == datetime.datetime.now().month
+    email = 'test@test.com'
 
-#     assert sut.get_app_server_by_token(app_server_to_save.get_token()) is not None
+    reset_password_to_save = ResetPassword(email)
+    sut = ResetPasswordPersistence(postgresql_db)
+    sut.save(reset_password_to_save)
+
+    reset_password_obtained = email = 'test@test.com'
+    assert reset_password_obtained is not None
+    assert reset_password_obtained.email == email
 
 # def test_delete_app_server_successfully(postgresql_db):
 #     session = postgresql_db.session
@@ -74,24 +75,3 @@ def test_save_reset_password_successfully(postgresql_db):
 #     sut = AppServerPersistence(postgresql_db)
 #     with pytest.raises(AppServerNotFoundException):
 #         user = sut.delete('5a3d6026-2f5c-4957-b52d-c094b50774db')
-
-# def test_get_all_app_servers_successfully(postgresql_db):
-#     session = postgresql_db.session
-#     create_appservers_table(session)
-#     assert query_first_app_server(session) is None
-
-#     app_server_to_save = AppServer()
-#     sut = AppServerPersistence(postgresql_db)
-#     sut.save(app_server_to_save)
-#     row = query_first_app_server(session)
-#     assert row is not None
-#     assert row[1].month == datetime.datetime.now().month
-
-#     app_server_to_save_2 = AppServer()
-#     sut = AppServerPersistence(postgresql_db)
-#     sut.save(app_server_to_save_2)
-#     row = query_first_app_server(session)
-#     assert row is not None
-
-#     app_servers_received = sut.get_all_app_servers()
-#     assert len(app_servers_received) == 2
