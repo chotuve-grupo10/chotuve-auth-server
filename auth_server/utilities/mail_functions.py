ddb61e9fdf16ebbdcf68d1389812baa66c58082f
Rodrigo Etchegaray <rorroeche@gmail.com>
2020-07-15 12:39:59 -0300

    Cambio un poco el mail

--- auth_server/utilities/mail_functions.py
+++ auth_server/utilities/mail_functions.py
@@ -2,7 +2,7 @@ from flask_mail import Mail, Message
 mail = Mail()
 
 def send_email_with_reset_password_token(user_email, token):
-	msg = Message("Cambiar contraseña",
+	msg = Message("Solicitud de cambio de contraseña",
 						recipients=[user_email],
-						body='Hola, este es tu codigo:{0}'.format(token))
+						body='Hola! Este es tu codigo: {0}'.format(token))
 	mail.send(msg)
